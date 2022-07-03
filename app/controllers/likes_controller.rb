9d083dacb9b56a91cf5c74714d89346eed60caec
AlvaroJTorres <mangetrone@gmail.com>
2022-07-03 02:12:31 -0500

    Add Likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @product.likes.create(user_id: current_user.id)
+
+    redirect_back fallback_location: products_path
+  end
+
+  def destroy
+    like = Like.find(params[:id])
+    like.destroy
+
+    redirect_back fallback_location: products_path
+  end
 end
