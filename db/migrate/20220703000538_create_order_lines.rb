91b03b02d464ce8420adf9e88022ae41df187fff
AlvaroJTorres <mangetrone@gmail.com>
2022-07-02 19:14:53 -0500

    Add migration for order_lines

---
+++ db/migrate/20220703000538_create_order_lines.rb
@@ -0,0 +1,12 @@
+class CreateOrderLines < ActiveRecord::Migration[7.0]
+  def change
+    create_table :order_lines do |t|
+      t.references :order, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity
+      t.decimal :total, precision: 5, scale: 2
+
+      t.timestamps
+    end
+  end
+end
