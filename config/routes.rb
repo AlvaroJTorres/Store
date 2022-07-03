5c77f42648170f03aadbf5ef64cbc67e2c24da57
AlvaroJTorres <mangetrone@gmail.com>
2022-07-03 01:06:07 -0500

    Configurate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
+
+  resources :products
+  # resources :users, only: [:show, :edit, :update]
 end
