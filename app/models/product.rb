1f51a98069a1deaefd83527ae4cae8a3cb32265c
AlvaroJTorres <mangetrone@gmail.com>
2022-07-02 21:06:57 -0500

    Add relation between product and likes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_lines, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
