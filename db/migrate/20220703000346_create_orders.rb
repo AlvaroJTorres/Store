e4abd8080673d51d7c84ae17a81103a50ad49f5f
AlvaroJTorres <mangetrone@gmail.com>
2022-07-02 19:14:40 -0500

    Add migration for orders

---
+++ db/migrate/20220703000346_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.date :date
+      t.references :user, null: false, foreign_key: true
+      t.decimal :total, precision: 6, scale: 2
+      t.integer :status, default: 0
+
+      t.timestamps
+    end
+  end
+end
