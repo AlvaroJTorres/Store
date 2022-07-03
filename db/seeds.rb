b685f046952f8354e4ab6d3510129fa4d3f07ca8
AlvaroJTorres <mangetrone@gmail.com>
2022-07-02 21:07:13 -0500

    Add seeds for project :seedling:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+
+products_data = JSON.parse(File.read('db/data/products.json'))
+
+puts 'Start seeding'
+
+puts 'Seeding users'
+admin_data = {
+  email: "alvaro@mail.com",
+  password: "123456",
+  first_name: "Alvaro",
+  last_name: "Torres",
+  address: Faker::Address.street_address,
+  phone: Faker::PhoneNumber.subscriber_number(length: 9),
+  role: 1
+}
+
+User.create(admin_data)
+
+customer_data = {
+  email: "test1@mail.com",
+  password: "123456",
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  address: Faker::Address.street_address,
+  phone: Faker::PhoneNumber.subscriber_number(length: 9)
+}
+
+User.create(customer_data)
+puts 'Finished seeding users'
+
+puts 'Seeding products'
+products_data.each do |product_data|
+  Product.create(products_data)
+end
+puts 'Finished seeding products'
