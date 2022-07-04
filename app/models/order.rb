629c12f27791a419f4034943f5c618d4804b9a48
AlvaroJTorres <mangetrone@gmail.com>
2022-07-04 00:04:19 -0500

    Set total for the order and displays it in the view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  before_save :set_total
   # belongs_to :user
   has_many :order_lines, dependent: :destroy
 
@@ -11,10 +12,17 @@ class Order < ApplicationRecord
     current_order_line = order_lines.find_by(product_id: params[:product_id])
     if current_order_line
       current_order_line.quantity += params[:quantity].to_i
-      current_order_line.save
+      
     else
       current_order_line = order_lines.new(params)
     end
+    current_order_line.save
     current_order_line
+  end
+
+  private
+
+  def set_total
+    self[:total] = order_lines.sum(&:total)
   end
 end
