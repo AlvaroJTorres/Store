a7ced98ff27f0179e4e98c040fc5b3a250a1d3b2
AlvaroJTorres <mangetrone@gmail.com>
2022-07-03 22:00:21 -0500

    Update model validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,18 @@
 class Order < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :order_lines, dependent: :destroy
 
-  validates :date, presence: true
-  validates :total, presence: true, numericality: { greater_than: 0 }
+  # validates :date, presence: true
+  # validates :total, presence: true, numericality: { greater_than: 0 }
 
   enum status: { recieved: 0, on_route: 1, delivered: 2 }
+
+  private
+
+  def self.set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
+  end
 end
--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,7 +1,15 @@
 class OrderLine < ApplicationRecord
+  before_save :set_total
+
   belongs_to :order
   belongs_to :product
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
-  validates :total, presence: true, numericality: { greater_than: 0 }
+  # validates :total, presence: true, numericality: { greater_than: 0 }
+
+  private
+
+  def set_total
+    self[:total] = product.price * quantity
+  end
 end
