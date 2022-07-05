300d2c71f6224450878a2f1dbc27c96b679b8b7a
AlvaroJTorres <mangetrone@gmail.com>
2022-07-05 18:42:31 -0500

    Update order_line model to calculate order total

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,15 +1,27 @@
 class OrderLine < ApplicationRecord
   before_save :set_total
+  after_update :update_total
+  after_destroy :update_total
 
   belongs_to :order
   belongs_to :product
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
-  # validates :total, presence: true, numericality: { greater_than: 0 }
+  
+  validate :check_stock
 
+  def check_stock
+    unless product.available_stock?(quantity)
+      errors.add(:quantity, :amount, message: "Not enough stock")
+    end 
+  end
   private
 
   def set_total
     self[:total] = product.price * quantity
+  end
+
+  def update_total
+    order.save
   end
 end
