37d9733a104399b833337d04ab3e3a260a95b72c
AlvaroJTorres <mangetrone@gmail.com>
2022-07-06 15:21:55 -0500

    Update routes to connect controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root "products#index"
+
+  devise_for :users, :controllers => { :sessions => "sessions"}
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "products#index"
 
   resources :products, shallow: true do
     resources :likes, only: [:create, :destroy]
   end
   resources :order_lines
+  resources :orders, only: [:update]
 
   get 'cart', to: 'orders#show'
 end
