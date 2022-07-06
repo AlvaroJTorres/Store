c2862928082e791c39b14223759b8d275f0c7316
AlvaroJTorres <mangetrone@gmail.com>
2022-07-06 15:19:44 -0500

    Add update method to save the order as bougth

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :set_order
+
   def show
     @order_lines = current_order.order_lines
+  end
+
+  def update
+    authorize @order
+    @order.date = Time.now
+    if @order.save
+      @order.order_lines.each do |order_line|
+        product = Product.find(order_line.product_id)
+        product.changed_by = current_user
+        product.stock -= order_line.quantity
+        product.save
+      end
+
+      session.delete(:order_id)
+      redirect_to products_path
+      
+    else
+      render :show
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:user_id, :date, :status)
+  end
+
+  def set_order
+    @order = current_order
   end
 end
