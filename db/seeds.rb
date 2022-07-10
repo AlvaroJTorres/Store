b69cf4e403a3343fc4ed5560ba74cee6a079ad4e
AlvaroJTorres <mangetrone@gmail.com>
2022-07-09 20:30:36 -0500

    Update seeds with tags and likes :seedling:

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@
 require 'json'
 
 products_data = JSON.parse(File.read('db/data/products.json'))
+tags_data = JSON.parse(File.read('db/data/tags.json'))
 
 puts 'Start seeding'
 
@@ -26,20 +27,55 @@ admin_data = {
 
 User.create(admin_data)
 
-customer_data = {
-  email: 'test1@mail.com',
-  password: '123456',
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  address: Faker::Address.street_address,
-  phone: Faker::PhoneNumber.subscriber_number(length: 9)
-}
+5.times do |i|
+  customer_data = {
+    email: "test#{i + 1}@mail.com",
+    password: '123456',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_address,
+    phone: Faker::PhoneNumber.subscriber_number(length: 9)
+  }
+  User.create(customer_data)
+end
 
-User.create(customer_data)
 puts 'Finished seeding users'
 
+puts 'Seeding tags'
+Tag.create(tags_data)
+puts 'Finished seeding tags'
+
 puts 'Seeding products'
-products_data.each do |_product_data|
-  Product.create(products_data)
+products_data.each do |product_data|
+  product = {
+    name: products_data[:name],
+    description: products_data[:description],
+    price: products_data[:price],
+    stock: products_data[:stock]
+  }
+
+  new_product = Product.create(product)
+
+  products_data[:tags].each do |tag|
+    new_product.tags << Tag.find_by(name: tag)
+  end
 end
+
 puts 'Finished seeding products'
+
+puts 'Seeding likes'
+product10 = Product.find(10)
+(2..6).each do |i|
+  product10.likes.create(user_id: i)
+end
+
+product15 = Product.find(15)
+(3..6).each do |i|
+  product15.likes.create(user_id: i)
+end
+
+product3 = Product.find(3)
+(2..4).each do |i|
+  product15.likes.create(user_id: i)
+end
+puts 'Finished seeding likes'
