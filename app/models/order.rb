f36a959e221bec61a5c113ebee1711129624ea26
AlvaroJTorres <mangetrone@gmail.com>
2022-07-09 22:08:07 -0500

    Add polymorphic association with commentable

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
   before_save :set_total
 
   has_many :order_lines, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   enum status: { in_process: 0, recieved: 1, on_route: 2, delivered: 3 }
 
