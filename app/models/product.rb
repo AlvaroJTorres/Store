4326b629e39643095de6348129b650cbadc0c085
AlvaroJTorres <mangetrone@gmail.com>
2022-07-09 20:02:58 -0500

    Add many to many association of products and tags

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   has_many :order_lines, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :logs, as: :loggable
+  has_and_belongs_to_many :tags
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
