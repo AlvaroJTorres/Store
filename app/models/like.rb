81a1619dafaebdd7ec7335b43c68b1c34e0f7e45
AlvaroJTorres <mangetrone@gmail.com>
2022-07-09 19:42:50 -0500

    Add counter_cache for likes on products

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@
 
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :product
+  belongs_to :product, counter_cache: :likes_count
 
   validates :user_id, uniqueness: { scope: :product_id }
 end
