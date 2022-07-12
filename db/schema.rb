79d6d23c3e2952f5e0076405332b4e0290d8725a
AlvaroJTorres <mangetrone@gmail.com>
2022-07-11 21:20:15 -0500

    Update schema with rubocop requirements

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,111 +12,111 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_10_025851) do
+ActiveRecord::Schema[7.0].define(version: 20_220_710_025_851) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.integer "rate", default: 0
-    t.bigint "user_id", null: false
-    t.string "commentable_type", null: false
-    t.bigint "commentable_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table 'comments', force: :cascade do |t|
+    t.text 'body'
+    t.integer 'rate', default: 0
+    t.bigint 'user_id', null: false
+    t.string 'commentable_type', null: false
+    t.bigint 'commentable_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[commentable_type commentable_id], name: 'index_comments_on_commentable'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "product_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_likes_on_product_id"
-    t.index ["user_id", "product_id"], name: "index_likes_on_user_id_and_product_id", unique: true
-    t.index ["user_id"], name: "index_likes_on_user_id"
+  create_table 'likes', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'product_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['product_id'], name: 'index_likes_on_product_id'
+    t.index %w[user_id product_id], name: 'index_likes_on_user_id_and_product_id', unique: true
+    t.index ['user_id'], name: 'index_likes_on_user_id'
   end
 
-  create_table "logs", force: :cascade do |t|
-    t.string "loggable_type"
-    t.bigint "loggable_id"
-    t.string "attr"
-    t.string "old_val"
-    t.string "new_val"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["loggable_type", "loggable_id"], name: "index_logs_on_loggable"
-    t.index ["user_id"], name: "index_logs_on_user_id"
+  create_table 'logs', force: :cascade do |t|
+    t.string 'loggable_type'
+    t.bigint 'loggable_id'
+    t.string 'attr'
+    t.string 'old_val'
+    t.string 'new_val'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[loggable_type loggable_id], name: 'index_logs_on_loggable'
+    t.index ['user_id'], name: 'index_logs_on_user_id'
   end
 
-  create_table "order_lines", force: :cascade do |t|
-    t.bigint "order_id", null: false
-    t.bigint "product_id", null: false
-    t.integer "quantity"
-    t.decimal "total", precision: 5, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_order_lines_on_order_id"
-    t.index ["product_id"], name: "index_order_lines_on_product_id"
+  create_table 'order_lines', force: :cascade do |t|
+    t.bigint 'order_id', null: false
+    t.bigint 'product_id', null: false
+    t.integer 'quantity'
+    t.decimal 'total', precision: 5, scale: 2
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['order_id'], name: 'index_order_lines_on_order_id'
+    t.index ['product_id'], name: 'index_order_lines_on_product_id'
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.date "date"
-    t.bigint "user_id"
-    t.decimal "total", precision: 6, scale: 2
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table 'orders', force: :cascade do |t|
+    t.date 'date'
+    t.bigint 'user_id'
+    t.decimal 'total', precision: 6, scale: 2
+    t.integer 'status', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_orders_on_user_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.decimal "price", precision: 5, scale: 2
-    t.integer "stock"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "likes_count", default: 0
+  create_table 'products', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.decimal 'price', precision: 5, scale: 2
+    t.integer 'stock'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'likes_count', default: 0
   end
 
-  create_table "products_tags", id: false, force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.bigint "tag_id", null: false
-    t.index ["product_id"], name: "index_products_tags_on_product_id"
-    t.index ["tag_id"], name: "index_products_tags_on_tag_id"
+  create_table 'products_tags', id: false, force: :cascade do |t|
+    t.bigint 'product_id', null: false
+    t.bigint 'tag_id', null: false
+    t.index ['product_id'], name: 'index_products_tags_on_product_id'
+    t.index ['tag_id'], name: 'index_products_tags_on_tag_id'
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'tags', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
-    t.string "phone"
-    t.integer "role", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'address'
+    t.string 'phone'
+    t.integer 'role', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "products"
-  add_foreign_key "likes", "users"
-  add_foreign_key "logs", "users"
-  add_foreign_key "order_lines", "orders"
-  add_foreign_key "order_lines", "products"
-  add_foreign_key "orders", "users"
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'likes', 'products'
+  add_foreign_key 'likes', 'users'
+  add_foreign_key 'logs', 'users'
+  add_foreign_key 'order_lines', 'orders'
+  add_foreign_key 'order_lines', 'products'
+  add_foreign_key 'orders', 'users'
 end
