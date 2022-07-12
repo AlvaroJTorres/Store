6cc2930a00fc066f506e4de15da6696ad01e218f
AlvaroJTorres <mangetrone@gmail.com>
2022-07-11 21:13:05 -0500

    Add flash alert for pundit NotAuthorizeError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(request.referrer || root_path)
+  end
+
   protected
 
   def configure_permitted_parameters
