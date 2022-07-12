d29a6bc330e67ceef37eb4f7626e6891ca5e8f5a
AlvaroJTorres <mangetrone@gmail.com>
2022-07-11 22:33:03 -0500

    Update styles for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
   def current_order
     Order.find_by_id(session[:order_id]).nil? ? Order.new : Order.find_by_id(session[:order_id])
   end
+
+  def button_style
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  end
 end
