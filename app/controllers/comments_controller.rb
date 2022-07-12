ed16c89e0ad8e0dcddb4b45aafdb76eacae6cac6
AlvaroJTorres <mangetrone@gmail.com>
2022-07-11 21:16:39 -0500

    Add controller for create new comments on commentable

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# Controllers for Comments
+class CommentsController < ApplicationController
+  before_action :set_commentable
+
+  def create
+    @comment = authorize @commentable.comments.new(comments_params)
+    @comment.user = current_user
+
+    flash[:alert] = "Comment wasn't created" unless @comment.save
+
+    redirect_to @commentable
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:body, :rate, :user_id)
+  end
+
+  def set_commentable
+    @commentable = if params[:product_id]
+                     Product.find(params[:product_id])
+                   elsif params[:order_id]
+                     Order.find(params[:order_id])
+                   end
+  end
+
+  def set_commentable_path
+    case @commentable.class.to_s
+    when 'Product'
+      product_path(@commentable)
+    when 'Order'
+      order_path(@commentable)
+    end
+  end
+end
---
+++ app/helpers/comments_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Helpers for Comments views
+module CommentsHelper
+end
