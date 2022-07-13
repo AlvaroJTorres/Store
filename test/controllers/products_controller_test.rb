475530631318e327f1aa44d0a44b547993f7c5ff
AlvaroJTorres <mangetrone@gmail.com>
2022-07-13 09:10:23 -0500

    Add tests for products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,7 +3,54 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user, role: 1)
+    @product = create(:product)
+  end
+
+  test 'should get index' do
+    get products_url
+    assert_response :success
+  end
+
+  test 'should show product' do
+    get product_url @product
+
+    assert_response :success
+  end
+
+  test 'should create product' do
+    sign_in @user
+
+    assert_difference('Product.count') do
+      post products_url, params: { product: { name: 'New Product', price: 10, stock: 100 } }
+    end
+
+    assert_redirected_to products_url
+  end
+
+  test 'should update product' do
+    sign_in @user
+
+    new_price = @product.price * 2
+    new_stock = @product.stock * 2
+
+    patch product_path @product, params: { product: { price: new_price, stock: new_stock } }
+
+    @product.reload
+
+    assert_equal(new_price, @product.price)
+
+    assert_redirected_to products_url
+  end
+
+  test 'sould destroy product' do
+    sign_in @user
+
+    assert_difference('Product.count', -1) do
+      delete product_url @product
+    end
+
+    assert_redirected_to products_url
+  end
 end
