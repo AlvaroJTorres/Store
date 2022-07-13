fa0dbf2ef63131f1de630bd38c126f15766025d2
AlvaroJTorres <mangetrone@gmail.com>
2022-07-13 09:09:44 -0500

    Add tests for likes

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -3,7 +3,30 @@
 require 'test_helper'
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    @like = create(:like, user: @user)
+  end
+
+  test 'should create like' do
+    sign_in @user
+
+    new_product = create(:product)
+
+    assert_difference('Like.count') do
+      post product_likes_path new_product
+    end
+
+    assert_redirected_to products_url
+  end
+
+  test 'should destroy like' do
+    sign_in @user
+
+    assert_difference('Like.count', -1) do
+      delete like_url @like
+    end
+
+    assert_redirected_to products_url
+  end
 end
