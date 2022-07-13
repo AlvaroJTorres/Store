6a74419f5ed6c141a20804ed50552bb54c8f26b5
AlvaroJTorres <mangetrone@gmail.com>
2022-07-13 09:09:57 -0500

    Add tests for logs

--- test/controllers/logs_controller_test.rb
+++ test/controllers/logs_controller_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class LogsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user, role: 1)
+    @log = create(:log, :for_product)
+  end
+
+  test 'should get index' do
+    sign_in @user
+
+    get logs_url
+
+    assert_response :success
+  end
 end
