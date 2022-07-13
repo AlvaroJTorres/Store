e3f09ea1d9500b57fee011307e9f3404e034c689
AlvaroJTorres <mangetrone@gmail.com>
2022-07-12 19:18:09 -0500

    Add custom reusable button style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,6 @@ module ApplicationHelper
   end
 
   def button_style
-    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
   end
 end
