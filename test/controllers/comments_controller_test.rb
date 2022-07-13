c8785d60e8672631f6647807bb88f626b30e888a
AlvaroJTorres <mangetrone@gmail.com>
2022-07-13 09:09:36 -0500

    Add tests for comments

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -3,7 +3,29 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    @order = create(:order, user_id: @user.id)
+    @product = create(:product)
+  end
+
+  test 'should create comment in product' do
+    sign_in @user
+
+    assert_difference('Comment.count') do
+      post product_comments_url @product, params: { comment: { body: 'Body', rate: 2 } }
+    end
+
+    assert_redirected_to @product
+  end
+
+  test 'should create comment in order' do
+    sign_in @user
+
+    assert_difference('Comment.count') do
+      post order_comments_url @order, params: { comment: { body: 'Body', rate: 2 } }
+    end
+
+    assert_redirected_to @order
+  end
 end
