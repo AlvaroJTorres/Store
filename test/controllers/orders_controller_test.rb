c3e4517f1c0810f22a54e88be7124336bdf93b7e
AlvaroJTorres <mangetrone@gmail.com>
2022-07-13 09:10:16 -0500

    Add tests for orders

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -3,7 +3,45 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    @order = create(:order, user_id: @user.id)
+    @product = create(:product)
+    @order_line = create(:order_line, order: @order, product: @product)
+  end
+
+  test 'should get index' do
+    sign_in @user
+
+    get orders_url
+    assert_response :success
+  end
+
+  test 'should show order' do
+    sign_in @user
+
+    get order_url @order
+
+    assert_response :success
+  end
+
+  test 'should show cart' do
+    sign_in @user
+
+    get cart_path
+
+    assert_response :success
+  end
+
+  test 'should update order' do
+    sign_in @user
+
+    patch order_url @order
+
+    @order.reload
+
+    assert_equal('recieved', @order.status)
+
+    assert_redirected_to products_url
+  end
 end
