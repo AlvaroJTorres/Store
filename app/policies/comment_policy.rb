1c4a3de1fcb7674d76a336af9ec778ae3dcb7c2b
AlvaroJTorres <mangetrone@gmail.com>
2022-07-17 15:05:31 -0500

    Update policies for show, update and destroy comments

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -3,7 +3,25 @@
 # Define policies for Comments Controllers
 class CommentPolicy < ApplicationPolicy
   # Policy for the create controller
+  class Scope < Scope
+    def resolve
+      if user&.admin? || user&.support?
+        scope.all
+      else
+        scope.where(approved: true).or(scope.where(user_id: user&.id))
+      end
+    end
+  end
+  
   def create?
     user&.customer?
+  end
+
+  def update?
+    user&.admin? || user&.support?
+  end
+
+  def destroy?
+    user&.admin?
   end
 end
