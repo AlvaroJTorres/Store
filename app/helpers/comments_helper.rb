f6b36bb802eb62dfc8f04214d0ebce96787196b0
AlvaroJTorres <mangetrone@gmail.com>
2022-07-17 15:05:57 -0500

    Add helpers to specify proper route for polymorphic comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,21 @@
 
 # Helpers for Comments views
 module CommentsHelper
+  def comment_approve_link(commentable, comment)
+    case commentable.class.to_s
+    when 'Product'
+      button_to 'Approve Comment', product_comment_path(commentable, comment), { method: :patch, class: button_style }
+    when 'Order'
+      button_to 'Approve Comment', order_comment_path(commentable, comment), { method: :patch, class: button_style }
+    end
+  end
+
+  def comment_delete_link(commentable, comment)
+    case commentable.class.to_s
+    when 'Product'
+      link_to 'Delete Comment', product_comment_path(commentable, comment), { method: :delete, class: button_style }
+    when 'Order'
+      link_to 'Delete Comment', order_comment_path(commentable, comment), { method: :delete, class: button_style }
+    end
+  end
 end
