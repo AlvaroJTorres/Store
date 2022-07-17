0620295e270c1d1a83716a7e6687960df0f2583a
AlvaroJTorres <mangetrone@gmail.com>
2022-07-17 02:21:35 -0500

    Update with rubocop ruling

--- app/queries/products_query.rb
+++ app/queries/products_query.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Queries for Products Model
 class ProductsQuery
   attr_reader :relation
 
@@ -5,24 +8,28 @@ class ProductsQuery
     @relation = relation
   end
 
+  # Method that orders the products by alphabetical order
   def order_by_letter(direction)
     return relation unless direction.present?
 
     relation.order("name #{direction}")
   end
 
+  # Method that orders the products by the amount of likes
   def order_by_likes_count(direction)
     return relation unless direction.present?
 
     relation.order("likes_count #{direction}")
   end
 
+  # Method that filters the list of products per name
   def filter_by_term(term)
     return relation unless term.present?
 
     relation.where('products.name LIKE ?', "%#{term}%")
   end
 
+  # Method that filters the list of products by its tags
   def filter_by_tag(tag)
     return relation unless tag.present?
 
