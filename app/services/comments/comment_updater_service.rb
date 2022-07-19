364a67e91623b217072622c57c42de20868cacd1
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:04:28 -0500

    Add service object to approve a comment

---
+++ app/services/comments/comment_updater_service.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Comments
+  # Service Object to Update a Comment
+  class CommentUpdaterService < ApplicationService
+    def initialize(comment)
+      @comment = comment
+    end
+
+    def call
+      approve_comment
+
+      @comment.save
+    end
+
+    private
+
+    def approve_comment
+      @comment.approved = true
+    end
+  end
+end
