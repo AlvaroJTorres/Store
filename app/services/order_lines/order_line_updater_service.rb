a564b1f917d167c93ebc3a5964cba32f0be87042
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:06:32 -0500

    Add service object to update a order line

---
+++ app/services/order_lines/order_line_updater_service.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module OrderLines
+  # Service Object to Update a Order Line
+  class OrderLineUpdaterService < ApplicationService
+    def initialize(order_line, params)
+      @order_line = order_line
+      @params = params
+    end
+
+    def call
+      @order_line.quantity = @params
+
+      @order_line.save
+    end
+  end
+end
