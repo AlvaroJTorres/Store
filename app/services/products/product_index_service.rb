7bdaf78c4cf090fffec0deb5c2f5f3b9aa200a0b
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:08:21 -0500

    Add service object to list all products

---
+++ app/services/products/product_index_service.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Products
+  # Service Object to List all Products
+  class ProductIndexService < ApplicationService
+    def initialize(params)
+      @params = params
+    end
+
+    def call
+      products_scope
+    end
+
+    private
+
+    # Method that uses the Product query object to order the products by alphabetical order and popularity
+    # and filters the products by letter or tags
+    def products_scope
+      ordered_by_name = ProductsQuery.new.order_by_letter(@params[:letter])
+      ordered_by_likes = ProductsQuery.new(ordered_by_name).order_by_likes_count(@params[:likes_count])
+      filter_by_name = ProductsQuery.new(ordered_by_likes).filter_by_term(@params[:term])
+      ProductsQuery.new(filter_by_name).filter_by_tag(@params[:tag])
+    end
+  end
+end
