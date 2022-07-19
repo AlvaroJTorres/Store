a4c34629dc9d2106c378fa985d12cf8729d4afc5
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:07:30 -0500

    Add service object to update an order

---
+++ app/services/orders/order_updater_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Orders
+  # Service Object to Update an Order to closed it for the user
+  class OrderUpdaterService < ApplicationService
+    def initialize(order, user)
+      @order = order
+      @user = user
+    end
+
+    def call
+      @order.user_id = @user.id if @order.user_id.nil?
+      @order.date = Time.now
+      @order.status = 1
+
+      return false unless @order.save
+
+      update_stock(@order, @user)
+    end
+
+    private
+
+    # Method that updates the stock of the products inside an order
+    def update_stock(order, user)
+      order.order_lines.each do |order_line|
+        product = Product.find(order_line.product_id)
+        product.changed_by = user
+        product.stock -= order_line.quantity
+        product.save
+      end
+    end
+  end
+end
