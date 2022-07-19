09ddac31f410eb43c4c204d68f655ce08f4bcbfa
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:10:17 -0500

    Add tests for the new endpoints of comments controller

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,8 +5,10 @@ require 'test_helper'
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = create(:user)
+    @admin_user = create(:user, role: 1)
     @order = create(:order, user_id: @user.id)
     @product = create(:product)
+    @comment = create(:comment, user: @user, commentable: @product)
   end
 
   test 'should create comment in product' do
@@ -27,5 +29,24 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to @order
+  end
+
+  test 'should approve comment' do
+    sign_in @admin_user
+
+    patch product_comment_path(@product, @comment)
+
+    @comment.reload
+    assert_equal(true, @comment.approved)
+  end
+
+  test 'should delete comment' do
+    sign_in @admin_user
+
+    assert_difference('Comment.count', -1) do
+      delete product_comment_path(@product, @comment)
+    end
+
+    assert_redirected_to @product
   end
 end
