6cc4c34dde23d4cd8b2048f13c9bb169830da607
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:09:37 -0500

    Update products controller with implemented services

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,18 +2,19 @@
 
 # Define the Controllers required for the Product endpoints
 class ProductsController < ApplicationController
+  before_action :pundit_authorize, except: %i[index show]
   before_action :set_product, only: %i[edit update destroy]
   before_action :new_order_line, only: %i[index show]
 
   # Method that responds to the get request to list all the records
   # of products
   def index
-    @products = products_scope
+    @products = Products::ProductIndexService.call(query_params)
   end
 
   # Method that responds to the get request to show an specific product
   def show
-    @product = Product.find(params[:id])
+    @product = Products::ProductFinderService.call(params[:id])
   end
 
   # Method that allows the render of the new form for a new product
@@ -23,10 +24,7 @@ class ProductsController < ApplicationController
 
   # Method that respond to the create request to create a new product
   def create
-    @product = Product.new(product_params)
-    authorize @product
-
-    if @product.save
+    if Products::ProductCreatorService.call(product_params)
       redirect_to products_path
     else
       render :new
@@ -39,9 +37,7 @@ class ProductsController < ApplicationController
   # Method that responds to the update request to check if the user is an admin
   # and changes the stock and price params
   def update
-    @product.changed_by = current_user
-
-    if @product.update(permitted_attributes(@product))
+    if Products::ProductUpdaterService.call(@product, permitted_attributes(@product), current_user)
       redirect_to products_path
     else
       render :edit
@@ -50,7 +46,8 @@ class ProductsController < ApplicationController
 
   # Method that responds to the delete request and removes a product from the database
   def destroy
-    @product.destroy
+    Products::ProductDestroyService.call(@product)
+
     redirect_to products_path
   end
 
@@ -60,15 +57,16 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name, :description, :price, :stock, :term, :tag, :letter, :likes_count)
   end
 
-  def products_scope
-    ordered_by_name_products = ProductsQuery.new.order_by_letter(params[:letter])
-    ordered_by_likes_products = ProductsQuery.new(ordered_by_name_products).order_by_likes_count(params[:likes_count])
-    filter_by_name_products = ProductsQuery.new(ordered_by_likes_products).filter_by_term(params[:term])
-    ProductsQuery.new(filter_by_name_products).filter_by_tag(params[:tag])
+  def query_params
+    params.slice(:term, :tag, :letter, :likes_count)
+  end
+
+  def pundit_authorize
+    authorize Product
   end
 
   def set_product
-    @product = authorize Product.find(params[:id])
+    @product = Products::ProductFinderService.call(params[:id])
   end
 
   def new_order_line
