8042619599ff4280637fa9e7e8488f40adc7dfd3
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:00:31 -0500

    Remove unused method add_product

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,22 +9,6 @@ class Order < ApplicationRecord
 
   enum status: { in_process: 0, recieved: 1, on_route: 2, delivered: 3 }
 
-  # Method that defines that if an order_line of a product already exists it
-  # updates the quantity value of the order_line, else it creates a new
-  # instance of an order_line for the product.
-  # Recieves all the params send to the order_line
-  def add_product(params)
-    current_order_line = order_lines.find_by(product_id: params[:product_id])
-    if current_order_line
-      current_order_line.quantity += params[:quantity].to_i
-
-    else
-      current_order_line = order_lines.new(params)
-    end
-    current_order_line.save
-    current_order_line
-  end
-
   private
 
   def set_total
