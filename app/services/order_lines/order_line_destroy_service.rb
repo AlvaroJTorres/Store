33753bf26897137a4d1eba34f272173683878d27
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:06:04 -0500

    Add service object to delete a order line

---
+++ app/services/order_lines/order_line_destroy_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module OrderLines
+  # Service Object to Destroy a Order Line
+  class OrderLineDestroyService < ApplicationService
+    def initialize(order_line)
+      @order_line = order_line
+    end
+
+    def call
+      @order_line.destroy
+    end
+  end
+end
