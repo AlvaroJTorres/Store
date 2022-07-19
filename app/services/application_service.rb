58b587fc009860eaa9792c84a03dee81cdebaab1
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:01:36 -0500

    Add applicationservice to abstract the call method

---
+++ app/services/application_service.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Class that abstracts the call method and inherits to every service object
+class ApplicationService
+  def self.call(*args)
+    new(*args).call
+  end
+end
