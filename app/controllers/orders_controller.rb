ce4b675a615299eb5be36e549599b5c0eec038a9
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:09:27 -0500

    Update orders controller with implemented services

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,16 @@
 
 # Define the Controllers required for the Order endpoints
 class OrdersController < ApplicationController
-  before_action :set_order, only: %i[update]
+  before_action :set_order, only: %i[show update]
 
   # Method that responds to the get request to list all the records
   # of orders from a logged user
   def index
-    @orders = Order.where(user_id: current_user.id, status: 'recieved')
+    @orders = Orders::OrderIndexService.call(current_user)
   end
 
   # Method that responds to the get request to show an specific order
-  def show
-    @order = Order.find(params[:id])
-  end
+  def show; end
 
   # Method that responds to the /cart endpoint to show the cart that is active
   # on the current session
@@ -26,11 +24,7 @@ class OrdersController < ApplicationController
   # adds the user doing the order, the date of the order and changes its status
   # to recieved so its no longer show on the cart, and delete its from the session
   def update
-    @order.user_id = current_user.id if @order.user_id.nil?
-    @order.date = Time.now
-    @order.status = 1
-    if @order.save
-      update_stock(@order)
+    if Orders::OrderUpdaterService.call(@order, current_user)
       session.delete(:order_id)
       redirect_to products_path
     else
@@ -45,15 +39,6 @@ class OrdersController < ApplicationController
   end
 
   def set_order
-    @order = authorize Order.find(params[:id])
-  end
-
-  def update_stock(order)
-    order.order_lines.each do |order_line|
-      product = Product.find(order_line.product_id)
-      product.changed_by = current_user
-      product.stock -= order_line.quantity
-      product.save
-    end
+    @order = authorize Orders::OrderFinderService.call(params[:id])
   end
 end
