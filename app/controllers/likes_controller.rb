eeceb6d07a46139dec379087554938b1f35ccaf5
AlvaroJTorres <mangetrone@gmail.com>
2022-07-19 18:09:01 -0500

    Update likes controller with implemented services

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,21 +2,25 @@
 
 # Define the Controllers required for the Likes endpoints
 class LikesController < ApplicationController
+  before_action :set_like, only: %i[destroy]
+
   # Method that responds to the create request for a new like
   def create
-    @product = Product.find(params[:product_id])
-    authorize @product.likes
-    @product.likes.create(user_id: current_user.id)
+    like = authorize Likes::LikeCreatorService.call(params[:product_id], current_user)
 
-    redirect_back fallback_location: products_path
+    redirect_back fallback_location: products_path if like.save
   end
 
   # Method that responds to the delete request to delete a like from a product
   def destroy
-    like = Like.find(params[:id])
-    authorize like
-    like.destroy
+    Likes::LikeDestroyService.call(@like)
 
     redirect_back fallback_location: products_path
+  end
+
+  private
+
+  def set_like
+    @like = authorize Likes::LikeFinderService.call(params[:id])
   end
 end
