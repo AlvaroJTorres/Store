7a27e7a6a6156e504062cadfa2d3b33e2c70a07a
AlvaroJTorres <mangetrone@gmail.com>
2022-07-26 23:26:05 -0500

    Add representer for products

---
+++ app/representers/product_representer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Representable decorator for Product JSON
+class ProductRepresenter < Representable::Decorator
+  include Representable::JSON
+
+  property :id
+  property :name
+  property :price
+  property :stock
+  property :avatar_url, exec_context: :decorator
+
+  def avatar_url
+    represented.avatar_url
+  end
+end
