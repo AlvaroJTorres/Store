7dedbca2ad05a50bd96840ccd299996d632e4e82
AlvaroJTorres <mangetrone@gmail.com>
2022-07-28 15:33:06 -0500

    Add mini_magic for varian processor

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Store
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.autoloader = :classic
+    config.active_storage.variant_processor = :mini_magick
 
     Rails.application.reloader.to_prepare do
       ActiveStorage::Blob
