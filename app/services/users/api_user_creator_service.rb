401cf518e0ac38f076eea5a6f35eaf126dcc16d4
AlvaroJTorres <mangetrone@gmail.com>
2022-08-01 16:54:52 -0500

    Add user creator service for user API

---
+++ app/services/users/api_user_creator_service.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Users
+  # Service Object to Create an User on API
+  class ApiUserCreatorService < ApplicationService
+    def initialize(params)
+      super()
+      @params = params
+    end
+
+    def call
+      byebug
+      user_form = UserForm.new(@params)
+
+      if user_form.valid?
+        byebug
+        new_user = user_form.create!
+
+        UserRepresenter.new(new_user)
+      else
+        user_form
+      end
+    end
+  end
+end
