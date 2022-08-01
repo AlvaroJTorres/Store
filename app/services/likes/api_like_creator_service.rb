b4cbc47dbaa9cc3eb3bf863d93f11ad6cd987f34
AlvaroJTorres <mangetrone@gmail.com>
2022-08-01 16:53:45 -0500

    Add job on create action of like

--- app/services/likes/api_like_creator_service.rb
+++ app/services/likes/api_like_creator_service.rb
@@ -5,7 +5,7 @@ module Likes
   class ApiLikeCreatorService < LikeCreatorService
     def call
       like = Like.create(product_id: @product_id, user_id: @user.id)
-
+      LikesCounterJob.perform_later(@product_id, @user)
       LikeRepresenter.new(like)
     end
   end
