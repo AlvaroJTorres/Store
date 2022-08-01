d4697e579d111d25d464249f15b224d962a3e5b8
AlvaroJTorres <mangetrone@gmail.com>
2022-07-31 19:18:00 -0500

    Add service to create a comment on API

---
+++ app/services/comments/api_comment_creator_service.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Comments
+  # Service Object to Create a Comment on API
+  class ApiCommentCreatorService < CommentCreatorService
+
+    def call
+      comment_form = CommentForm.new(@params)
+
+      if comment_form.valid?
+        comment = @commentable.comments.new(@params)
+        comment.user = @user
+
+        comment.save
+        CommentRepresenter.new(comment)
+      end
+    end
+  end
+end
