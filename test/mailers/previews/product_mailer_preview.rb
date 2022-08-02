af0319858786661d2ab95e60935b240996ad8e31
AlvaroJTorres <mangetrone@gmail.com>
2022-08-02 13:07:56 -0500

    Add preview for mailers

---
+++ test/mailers/previews/product_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/product_mailer
+class ProductMailerPreview < ActionMailer::Preview
+  def alert_stock_email
+    ProductMailer.with(product: Product.find(15)).alert_stock_email
+  end
+end
