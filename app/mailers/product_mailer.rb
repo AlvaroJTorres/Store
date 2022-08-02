6f88013f1944509fc975a57f007104048e593d9e
AlvaroJTorres <mangetrone@gmail.com>
2022-08-02 13:04:32 -0500

    Add product mailer

---
+++ app/mailers/product_mailer.rb
@@ -0,0 +1,7 @@
+class ProductMailer < ApplicationMailer
+  def alert_stock_email
+    @product = params[:product]
+    @user = @product.likes.last.user
+    mail(to: @user.email, subject: 'The stock is running low')
+  end
+end
