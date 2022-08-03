691a4645a4870934c20e985efe4ff238b3c32b8d
AlvaroJTorres <mangetrone@gmail.com>
2022-08-03 14:17:16 -0500

    Update url option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = '???'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
   config.action_mailer.delivery_method = :smtp
 
