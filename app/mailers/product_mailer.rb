00e7940b13df021549ca9dc9813c29ecc885daec
AlvaroJTorres <mangetrone@gmail.com>
2022-08-03 17:24:51 -0500

    Update mailer to attach image

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -4,6 +4,7 @@
 class ProductMailer < ApplicationMailer
   def alert_stock_email
     @product = params[:product]
+    attachments['product.png'] = { mime_type: 'image/png', content: @product.avatar.download } if @product.avatar.attached?
     @user = @product.likes.last.user
     mail(to: @user.email, subject: 'The stock is running low')
   end
