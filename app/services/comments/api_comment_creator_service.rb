a42aa5e4ebaab24844ed2d5506930f9fc138e295
AlvaroJTorres <mangetrone@gmail.com>
2022-08-04 15:52:35 -0500

    Update comment creator to return errors

--- app/services/comments/api_comment_creator_service.rb
+++ app/services/comments/api_comment_creator_service.rb
@@ -4,15 +4,18 @@ module Comments
   # Service Object to Create a Comment on API
   class ApiCommentCreatorService < CommentCreatorService
     def call
-      comment_form = CommentForm.new(@params)
+      comment_form = ApiCommentForm.new(@params.merge(user_id: @user.id))
 
-      return unless comment_form.valid?
+      if comment_form.valid?
 
       comment = @commentable.comments.new(@params)
       comment.user = @user
 
       comment.save
       CommentRepresenter.new(comment)
+      else
+        comment_form
+      end
     end
   end
 end
