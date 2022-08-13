fd44a8b337c963cc89c4a681d1412c710cfad779
AlvaroJTorres <mangetrone@gmail.com>
2022-08-12 23:26:55 -0500

    Add previews for order mailers

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -2,4 +2,11 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
+    def successful_purchase
+        OrderMailer.with(customer: User.find(3), order: Order.first).successful_purchase
+      end
+    
+      def failed_purchase
+        OrderMailer.with(customer: User.find(3)).failed_purchase
+      end
 end
