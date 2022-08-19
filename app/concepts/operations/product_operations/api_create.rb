e8d384fe63518fa50473d40077812faec8bad210
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:27:23 -0500

    Add operation to create a product on api

---
+++ app/concepts/operations/product_operations/api_create.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Operations
+  module ProductOperations
+    class ApiCreate < Trailblazer::Operation
+      pass :new_model
+      step :validate_product
+      fail :validations_errors!
+      step :save_model
+      step :representer
+
+      def new_model(options, **)
+        options[:model] = Product.new
+      end
+
+      def validate_product(options, params:, **)
+        form = Forms::ProductCreateForm.new(options[:model])
+        options[:form] = form
+        form.validate(params)
+      end
+
+      def validations_errors!(options, **)
+        raise CustomError.new(options[:form].errors.messages)
+      end
+
+      def save_model(options, params:, **)
+        options[:model].attributes = params
+        options[:model].save
+      end
+
+      def representer(options, model:, **)
+        options[:model] = Representers::ProductRepresenter.new(model)
+      end
+    end
+  end
+end
