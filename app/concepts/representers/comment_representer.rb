5a5cc922344e9d63689642c6543733e6da4f3ccd
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:31:13 -0500

    Add representer for comments operations

---
+++ app/concepts/representers/comment_representer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Representers
+# Representable decorator for Comment JSON
+    class CommentRepresenter < Representable::Decorator
+        include Representable::JSON
+    
+        property :id
+        property :body
+        property :rate
+        property :user_id
+        property :approved
+        property :comment_author, exec_context: :decorator
+    
+        def comment_author
+            User.find(represented.user_id).first_name
+        end
+    end
+end
