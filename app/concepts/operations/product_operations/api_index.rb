bef0083b53fed0550bd239bfb20e690fbb69fcd5
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:27:42 -0500

    Add operation for indexing products

---
+++ app/concepts/operations/product_operations/api_index.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Operations
+  module ProductOperations
+    class ApiIndex < Trailblazer::Operation
+      step :products_scope
+
+      def products_scope(options, params:, **)
+        ordered_by_name = Querys::ProductsQuery.new.order_by_letter(params[:letter])
+        ordered_by_likes = Querys::ProductsQuery.new(ordered_by_name).order_by_likes_count(params[:likes_count])
+        filter_by_name = Querys::ProductsQuery.new(ordered_by_likes).filter_by_term(params[:term])
+        options[:model] = Querys::ProductsQuery.new(filter_by_name).filter_by_tag(params[:tag])
+      end
+    end
+  end
+end
