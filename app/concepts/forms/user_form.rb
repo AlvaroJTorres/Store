2f293d97a99190a5a5777b24e3b09c7b2e9aed79
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:22:58 -0500

    Add reform object for users

---
+++ app/concepts/forms/user_form.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Forms
+    # Form object for User Model
+    class UserForm < Reform::Form
+        property :email
+        property :password
+        property :first_name
+        property :last_name
+        property :address
+        property :phone
+        property :role
+    
+        validates :email, :password, :first_name, :last_name, :address, :phone, presence: true
+        validates :role, inclusion: { in: [0, 2] }
+    end
+end
