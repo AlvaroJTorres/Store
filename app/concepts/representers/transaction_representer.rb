5652ecec3ac91380c583c14e855ba1e21abefd06
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:31:51 -0500

    Add representer for transaction operations

---
+++ app/concepts/representers/transaction_representer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Representers
+    # Representable decorator for Transaction JSON
+    class TransactionRepresenter < Representable::Decorator
+        include Representable::JSON
+    
+        property :id
+        property :status
+        property :user_id
+        property :email, exec_context: :decorator
+        property :first_name, exec_context: :decorator
+        property :last_name, exec_context: :decorator
+        property :order_id
+        property :total, exec_context: :decorator
+    
+        def total
+        represented.order.total
+        end
+    
+        def email
+        represented.user.email
+        end
+    
+        def first_name
+        represented.user.first_name
+        end
+    
+        def last_name
+        represented.user.last_name
+        end
+    end
+end
