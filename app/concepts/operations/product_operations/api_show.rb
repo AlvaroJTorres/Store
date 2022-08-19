f8618f700fe676b07dc6904366f244922abf99d0
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:27:52 -0500

    Add operation to show a product on api

---
+++ app/concepts/operations/product_operations/api_show.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Operations
+  module ProductOperations
+    class ApiShow < Trailblazer::Operation
+      pass :find_product
+      step :representer
+
+      def find_product(options, params:, **)
+        options[:model] = Product.find_by(id: params)
+      end
+
+      def representer(options, model:, **)
+        options[:model] = Representers::ProductRepresenter.new(model)
+      end
+    end
+  end
+end
