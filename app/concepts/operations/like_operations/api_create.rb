0ccc848ecb0118b70a5eebb069a29ac0787612d0
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:24:20 -0500

    Add operation for creating a like on api

---
+++ app/concepts/operations/like_operations/api_create.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Operations
+    module LikeOperations
+      class ApiCreate < Trailblazer::Operation
+        step :create_like
+        step :representer
+
+        def create_like(options, params:, user:, **)
+            options[:model] = Like.create(product_id: params, user_id: user.id)
+            LikesCounterJob.perform_later(params, user)
+        end
+  
+        def representer(options, model:, **)
+          options[:model] = Representers::LikeRepresenter.new(model)
+        end
+      end
+    end
+end
