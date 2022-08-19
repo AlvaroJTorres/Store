a375fdd2c9848f6d451863aad6e36658336204d5
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:27:33 -0500

    Add operation to delete a product on api

---
+++ app/concepts/operations/product_operations/api_delete.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Operations
+  module ProductOperations
+    class ApiDelete < Trailblazer::Operation
+      pass :find_product
+      step :delete_product
+
+      def find_product(options, params:, **)
+        options[:model] = Product.find_by(id: params)
+      end
+
+      def delete_product(options, user:, **)
+        options[:model].changed_by = user
+        options[:model].deleted_at = Time.now
+        options[:model].save
+      end
+    end
+  end
+end
