81d8be33e3b3e14f246d56d441801aa1fbed848e
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:31:57 -0500

    Add representer for user operations

---
+++ app/concepts/representers/user_representer.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Representers
+  # Representable decorator for User JSON
+  class UserRepresenter < Representable::Decorator
+    include Representable::JSON
+  
+    property :id
+    property :email
+    property :first_name
+    property :last_name
+    property :role
+    property :deleted_at
+  end
+end
