35f78eab575f067aa5aab0503f4c8517242fc729
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:22:40 -0500

    Remove commented custom validation

--- app/concepts/forms/product_create_form.rb
+++ app/concepts/forms/product_create_form.rb
@@ -1,26 +1,16 @@
 # frozen_string_literal: true
 
 module Forms
-    # Form object for Product Model
-    class ProductCreateForm < Reform::Form    
-        property :name
-        property :description
-        property :price
-        property :stock
-        property :avatar
-    
-        validates :name, presence: true
-        validates :price, :stock, presence: true, numericality: { greater_than: 0 }
-        validates_uniqueness_of :name
-        # validate :unique_name
-    
-        # # Method that validates uniqueness of a name
-        # def unique_name
-        # return unless name
-    
-        # errors.add(:unique_name, 'Name already taken') if name.downcase == Product.find_by(name:)&.name
-        # end
-    end
-end
+  # Form object for Product Model
+  class ProductCreateForm < Reform::Form
+    property :name
+    property :description
+    property :price
+    property :stock
+    property :avatar
 
-  
+    validates :name, presence: true
+    validates :price, :stock, presence: true, numericality: { greater_than: 0 }
+    validates_uniqueness_of :name
+  end
+end
