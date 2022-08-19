fc6030ca15b523ba92cccee22b59c5a5ed8135b4
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 18:25:32 -0500

    Update custom errors json presentation

--- app/errors/error_handler.rb
+++ app/errors/error_handler.rb
@@ -6,10 +6,21 @@ module ErrorHandler
     end
 
     def custom_error(exception)
-        render_json_error(exception, '422')
+        render_json_error(errors_array(exception.errors), exception.status)
     end
 
     def render_json_error(errors, status)
         render json: { errors: errors }, status: status
+    end
+
+    def errors_array(errors)
+        err_arr = []
+        errors.each do |field, messages|
+            messages.each do |message|
+                json = {field_name: field, message: message}
+                err_arr << json
+            end  
+        end
+        err_arr
     end
 end
