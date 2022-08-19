cc4b63125b53b4c30191a490b355ab74d277674d
AlvaroJTorres <mangetrone@gmail.com>
2022-08-19 13:23:39 -0500

    Add operation for updating a comment on api

---
+++ app/concepts/operations/comment_operations/api_update.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Operations
+    module CommentOperations
+        class ApiUpdate < Trailblazer::Operation
+            pass :find_comment
+            step :approve_comment
+            step :representer
+
+            def find_comment(options, params:, commentable:, **)
+                options[:model] = commentable.comments.find_by(id: params)
+            end
+
+            def approve_comment(options, **)
+                options[:model].approved = true
+                options[:model].save
+            end
+
+            def representer(options, model:, **)
+                options[:model] = Representers::CommentRepresenter.new(model)
+            end
+        end
+    end
+end
